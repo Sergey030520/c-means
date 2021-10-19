9946aae320e79aa1b872868697e14bc0b58e2ec5
sergeimakarov <Malipa1941@yandex.ru>
2021-10-19 22:41:17 +0300

    git initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        C_Means c_means = new C_Means("src/com/company/dataset" ,3, 0.4, 1.5);
+    }
+}
